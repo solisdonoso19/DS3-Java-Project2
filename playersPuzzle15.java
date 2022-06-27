6d4f99483395c1e2595d8cf37088850454a8285b
Carlos Iván Solís Donoso <72238597+solisdonoso19@users.noreply.github.com>
2022-06-27 16:33:14 -0500

    Clase de objeto jugador

---
+++ playersPuzzle15.java
@@ -0,0 +1,20 @@
+public class playersPuzzle15 {
+    protected String player;
+    protected Integer timePlay;
+
+    public void setPlayer(String player) {
+        this.player = player;
+    }
+
+    public void setTimePlay(Integer timePlay) {
+        this.timePlay = timePlay;
+    }
+
+    public String getPlayer() {
+        return player;
+    }
+
+    public Integer getTimePlay() {
+        return timePlay;
+    }
+}
